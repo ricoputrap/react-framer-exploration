a3db08353e4b77a5fa3e181e4cde855b60f83293
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-11-06 16:31:53 +0700

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div className="flex flex-col gap-10 overflow-x-hidden">
+      <section className="grid grid-cols-3 p-10 gap-10">
+        <div className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"></div>
+        <div className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"></div>
+        <div className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"></div>
+        <div className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"></div>
+        <div className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"></div>
+        <div className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"></div>
+      </section>
+    </div>
+  )
+}
+
+export default App
